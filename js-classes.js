d3bcb58f52e6875b084543680898c3860c20af9c
Sashankr <sashankr97@gmail.com>
2024-01-24 08:19:43 +0530

    class declaration in ES6

--- js-classes.js
+++ js-classes.js
@@ -1,4 +1,5 @@
 /*Javascript is a prototype based programming language
+
 Javascript was not designed for object oriented programming, but as the language got popular, in ES6 Classes were introduced
 
 Classes in Javascript do bring a new object oriented inheritence model, but internally use the same prototypical inheritence
@@ -52,6 +53,27 @@ function person(name,age){
 }
 
 const sashank = new person("Sashank",26);
+console.log("Before ES6");
 console.log(sashank.details());
 
+// After ES6
+
+// Two ways to create a class
+// 1. Class Declaration
+// 2. Class Expression
+
+class Individual{
+
+constructor(name,age){
+ this.name = name;
+ this.age = age;
+}
+details(){
+return `${this.name}'s age is ${this.age}`
+}
+}
+
+const personal= new Individual("Sashank",26);
+console.log("ES6 Class Declaration");
+console.log(personal.details());
 
